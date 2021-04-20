879acc524825a650df59a8d21b925a83c48c9c56
Matheus <matt.marq1@gmail.com>
2021-04-19 22:38:33 -0300

    Revisão do código e divisão do database

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-import pandas 
+import pandas
 from sklearn import linear_model
 import sqlalchemy
 import requests
@@ -9,50 +9,34 @@ import csv
 import configparser
 
 config = configparser.ConfigParser()
-config.read("config.ini")
-#response = json.loads(requests.get(config.get("DEFAULT", "API_URL")).text)
-response = json.loads(requests.get("https://api.gemini.com/v2/ticker/btcusd").text)
+config.read("backend/config.ini")
+response = json.loads(requests.get(config.get("DEFAULT", "API_URL")).text)
 
 if os.path.isfile('backend/trained_model'):
     with open('backend/trained_model', 'rb') as file:
         model = pickle.load(file)
 
 else:
-    # df = pandas.read_csv("BASE_URL")
-    database = pandas.read_csv("https://www.cryptodatadownload.com/cdd/gemini_BTCUSD_1hr.csv", header=1)
-    database['Date'] = pandas.to_datetime(database['Date'])
-    database = database.sort_values(by='Date')
-    colunas_x = ['Open','High','Low']
-    X = database.loc[:, colunas_x]
+    database = pandas.read_csv(config.get(
+        "DEFAULT", "API_DATABASE_1H"), header=1)
+
+    X = database.loc[:, ['Open', 'High', 'Low']]
     Y = database.loc[:, ['Close']]
-    model = linear_model.LinearRegression()
-    model.fit(X, Y)
+
+    model = linear_model.LinearRegression().fit(X, Y)
+
     with open('backend/trained_model', 'wb') as file:
         pickle.dump(model, file)
-    #salvando o banco
-    database.to_csv('backend/database_1hr.csv')
 
-# X = list(csv.reader(open('D:\Downloads\gemini_BTCUSD_1hr_editado.csv')))
-#Puxando da api e prevendo com o modelo gerado
-response = requests.get("https://api.gemini.com/v2/candles/btcusd/1hr")
-btc_new_data = json.loads(response.text)
-btc_new_data.reverse()
-iterator = iter(btc_new_data)
-for i in iterator:
-        try:
-            #print(next(iterator))
-            i = next(iterator)
-            oopen = i[1]
-            high = i[2]
-            low = i[3]
-            close = i[4]
-            close_prediction = model.predict([[oopen, high, low]])
-            print("open:", oopen, "high:", high, "low:", low, "close:", close, "Previsão:", close_prediction,
-            "Diferença:", close - close_prediction)
-        except:
-            print('O último fechamento do Bitcoin: ', close_prediction)
+    validation_base = database.sample(frac=.2).loc[:, ['Open', 'High', 'Low']]
 
-# Y_new = model.predict(X)
+    database.to_csv('backend/gemini_database_1hr.csv', index=False)
+    validation_base.to_csv(
+        'backend/gemini_database_1hr_testing.csv', index=False)
 
-# for i in range(len(X)):
-#     print(f"Linha {i} - {X[i]} Previsão do Close = {Y_new[i]}")
+X = pandas.read_csv('backend/gemini_database_1hr_testing.csv').values.tolist()
+
+Y_new = model.predict(X)
+
+for i in range(len(X)):
+    print(f"Linha {i} - {X[i]} Previsão do Close = {Y_new[i]}")
